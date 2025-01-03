828501fe0659e998cd7165d2ce547b5b385e17ed
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-01-02 19:17:30 -0600

    feat: base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,4 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: "https://tresabhi.github.io/",
-  base: "professional-website",
 });
