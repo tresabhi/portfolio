a93a1d2dedc3fff21b0ca2299bb7019860c26a54
tresabhi <43380238+tresabhi@users.noreply.github.com>
2025-01-06 13:22:28 -0600

    proj

---
+++ src/components/Project/index.tsx
@@ -0,0 +1,158 @@
+import { PlayIcon } from "@radix-ui/react-icons";
+import {
+  Badge,
+  Box,
+  Flex,
+  Heading,
+  IconButton,
+  Link,
+  SegmentedControl,
+  Text,
+} from "@radix-ui/themes";
+import { useEffect, useState, type ReactNode } from "react";
+import { imgur } from "../../core/imgur";
+import { TimelineProjectType as ProjectType } from "./constants";
+
+interface ProjectProps {
+  title: string;
+  children: ReactNode;
+  time?: string;
+  type: ProjectType;
+  images?: string[];
+  video?: { image: string; youtube: string };
+  skills?: string[];
+}
+
+export function Project({
+  title,
+  children,
+  time,
+  type,
+  images,
+  video,
+  skills,
+}: ProjectProps) {
+  const [imageIndex, setImageIndex] = useState(0);
+  const [closeup, setCloseup] = useState(false);
+  const [cycle, setCycle] = useState(true);
+
+  useEffect(() => {
+    if (!cycle) return;
+
+    const interval = setInterval(() => {
+      setImageIndex((index) => (index + 1) % (images?.length ?? 1));
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [cycle]);
+
+  return (
+    <Flex gap="6">
+      <Flex ml="9" direction="column" gap="4" flexGrow="1" flexBasis="0">
+        <Flex direction="column">
+          <Flex align="center" gap="3">
+            <Heading>{title}</Heading>
+
+            {type === ProjectType.Educational && (
+              <Badge color="amber">Educational</Badge>
+            )}
+            {type === ProjectType.Club && <Badge color="green">Club</Badge>}
+            {type === ProjectType.Hobby && <Badge color="pink">Hobby</Badge>}
+          </Flex>
+
+          <Text color="gray">{time}</Text>
+        </Flex>
+
+        <Flex direction="column" gap="2">
+          {typeof children === "string" ? <Text>{children}</Text> : children}
+        </Flex>
+
+        {skills !== undefined && (
+          <Flex gap="1">
+            {skills.map((skill) => (
+              <Badge color="gray">{skill}</Badge>
+            ))}
+          </Flex>
+        )}
+      </Flex>
+
+      <Flex
+        flexGrow="1"
+        direction="column"
+        flexBasis="0"
+        align="center"
+        justify="center"
+        gap="2"
+        position="relative"
+      >
+        {(video || images) &&
+          (() => {
+            const box = (
+              <Box
+                style={{
+                  cursor: "pointer",
+                  backgroundImage: `url(${imgur(
+                    images ? images[imageIndex] : video!.image
+                  )})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center",
+                  width: "20rem",
+                  height: "20rem",
+                  boxShadow: "var(--shadow-2)",
+                  borderRadius: "var(--radius-2)",
+                }}
+                onClick={() => setCloseup(true)}
+              />
+            );
+
+            if (video)
+              return (
+                <Link
+                  href={`https://youtu.be/${video.youtube}`}
+                  target="_blank"
+                >
+                  {box}
+                </Link>
+              );
+
+            return box;
+          })()}
+
+        {images && (
+          <SegmentedControl.Root
+            size="1"
+            value={`${imageIndex}`}
+            onValueChange={(value) => {
+              setImageIndex(parseInt(value));
+              setCycle(false);
+            }}
+          >
+            {images.map((_, index) => (
+              <SegmentedControl.Item value={`${index}`}>
+                {index + 1}
+              </SegmentedControl.Item>
+            ))}
+          </SegmentedControl.Root>
+        )}
+
+        {video && (
+          <IconButton
+            size="4"
+            variant="surface"
+            style={{
+              cursor: "pointer",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              pointerEvents: "none",
+            }}
+            tabIndex={-1}
+          >
+            <PlayIcon />
+          </IconButton>
+        )}
+      </Flex>
+    </Flex>
+  );
+}
--- src/components/SectionProjects.tsx
+++ src/components/SectionProjects.tsx
@@ -1,7 +1,7 @@
-import { Flex, Heading, Separator, Text } from "@radix-ui/themes";
+import { Flex, Heading, Link, Quote, Separator, Text } from "@radix-ui/themes";
 import { ASPDoodle1, ASPDoodle2, ASPDoodle3, ASPDoodle4 } from "./ASPDoodle";
-import { TimelineProject } from "./TimelineProject";
-import { TimelineProjectType } from "./TimelineProject/constants";
+import { Project } from "./Project";
+import { TimelineProjectType } from "./Project/constants";
 
 const lorem =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -12,98 +12,175 @@ export function SectionProjects() {
       <Separator size="4" id="projects" />
 
       <Heading size="8" align="center">
-        Projects
+        Key Projects
       </Heading>
 
-      <Flex direction="column">
-        <TimelineProject
-          title="Cloud Surfers"
-          type={TimelineProjectType.Educational}
-          time="2024"
-          first
-        >
-          {lorem}
-        </TimelineProject>
+      <Project
+        title="J.E.T."
+        type={TimelineProjectType.Club}
+        time="2024 - present"
+        images={["dFofBwn", "3Wo1TaL"]}
+        skills={[
+          "Advanced Thermodynamics",
+          "Computer Aided Design",
+          "Computer Fluid Dynamics",
+        ]}
+      >
+        <Text>
+          I am responsible for creating the heart of our jet engine as the lead
+          designer for the combustion chamber of J.E.T. (Jet Engine Team, a
+          humorously unexacting name I coined under the Advanced Manufacturing
+          Club).
+        </Text>
+        <Text>
+          This project draws knowledge beyond my current coursework; hence, I
+          primarily rely on independent research. I am honing the combustion
+          chamber to achieve an optimal stoichiometric combustion at a precise
+          37:2 ratio. My design focuses on ease of manufacturing while
+          maintaining maximum efficiency to lower emissions.
+        </Text>
+      </Project>
 
-        <TimelineProject
-          title="J.E.T."
-          type={TimelineProjectType.Club}
-          time="2024 - present"
-        >
-          {lorem}
-        </TimelineProject>
+      <Project
+        title="Nerdy Birds"
+        type={TimelineProjectType.Club}
+        time="2019 - 2023"
+        video={{
+          image: "TcC9m7e",
+          youtube: "mjuX8Oxbl0k",
+        }}
+      >
+        <Text>
+          As the team captain for 2023 and programmer for the Nerdy Birds{" "}
+          <Link
+            target="_blank"
+            href="https://www.firstinspires.org/robotics/ftc"
+          >
+            FTC team
+          </Link>
+          , I led us to a decisive first-place victory at districts—our best
+          performance in the team's 10-year history—and a strong finish at
+          states.
+        </Text>
+        <Text>
+          With no prior guidance or existing solutions, I took the initiative to
+          research and implement advanced technologies for our robot's
+          autonomous mode. I mastered{" "}
+          <Link
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/Computer_visions"
+          >
+            computer vision
+          </Link>
+          ,{" "}
+          <Link
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/Pathfinding"
+          >
+            pathfinding
+          </Link>
+          ,{" "}
+          <Link
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/Inertial_navigation_system"
+          >
+            inertial navigation
+          </Link>
+          , and{" "}
+          <Link
+            target="_blank"
+            href="https://en.wikipedia.org/wiki/Mecanum_wheel"
+          >
+            mecanum omnidirectional traversal
+          </Link>
+          , pushing both my high school knowledge and the boundaries of what's
+          possible in FTC.
+        </Text>
+      </Project>
 
-        <TimelineProject
-          title="F-4 Phantom II Digitalization"
-          type={TimelineProjectType.Educational}
-          time="2023"
-        >
-          {lorem}
-        </TimelineProject>
+      <Project
+        title="Cloud Surfers"
+        type={TimelineProjectType.Educational}
+        time="2024"
+        images={["vFL12f7", "hlfxY5G"]}
+      >
+        {lorem}
+      </Project>
 
-        <TimelineProject
-          title="Avionics in Rocketry Club"
-          type={TimelineProjectType.Club}
-          time="2023"
-        >
-          {lorem}
-        </TimelineProject>
+      <Project
+        title="Abhi's Space Program"
+        type={TimelineProjectType.Hobby}
+        time="2013 - 2017"
+      >
+        <Text>
+          Abhi's Space Programs (ASP for short) marked my first venture into
+          propulsion, armed with zero formal knowledge of rocketry—or safety,
+          for that matter.
+        </Text>
+        <Text>
+          With aluminum foil bodies and solid motor fuel from sparklers and
+          candle wax, these rockets were truly the pinnacle of childhood
+          engineering! For inexplicable reasons, however, nearly every launch
+          followed the same familiar sequence:
+        </Text>
+      </Project>
 
-        <TimelineProject
-          title="Nerdy Birds"
-          type={TimelineProjectType.Club}
-          time="2019-2023"
-        >
-          {lorem}
-        </TimelineProject>
+      <Flex direction="column" align="center" gap="4">
+        <Flex wrap="wrap" gap="2" justify="center">
+          <Flex gap="2" wrap="wrap" justify="center">
+            <ASPDoodle1 width="17rem" height="17rem" />
+            <ASPDoodle2 width="17rem" height="17rem" />
+          </Flex>
+          <Flex gap="2" wrap="wrap" justify="center">
+            <ASPDoodle3 width="17rem" height="17rem" />
+            <ASPDoodle4 width="17rem" height="17rem" />
+          </Flex>
+        </Flex>
 
-        <TimelineProject
-          title="BlitzKit"
-          type={TimelineProjectType.Hobby}
-          time="2022 - present"
-        >
-          {lorem}
-        </TimelineProject>
-
-        <TimelineProject
-          title="Stellar"
-          type={TimelineProjectType.Hobby}
-          time="2021 - 2024"
-        >
-          My first proper endeavour into cyber-physical systems was the creation
-          of my own CAD software.
-        </TimelineProject>
-
-        <TimelineProject
-          title="Abhi's Space Program"
-          type={TimelineProjectType.Hobby}
-          time="2013 - 2017"
-          last
-        >
-          <Text>
-            Abhi's Space Programs (ASP for short) marked my first venture into
-            propulsion, armed with zero formal knowledge of rocketry—or safety,
-            for that matter.
-          </Text>
-          <Text>
-            With aluminum foil bodies and solid motor fuel from sparklers and
-            candle wax, these rockets were truly the pinnacle of childhood
-            engineering! For inexplicable reasons, however, nearly every launch
-            followed the same familiar sequence:
-          </Text>
-        </TimelineProject>
+        <Quote>Some jokes just don't take-off.</Quote>
       </Flex>
 
-      <Flex wrap="wrap" gap="2" justify="center">
-        <Flex gap="2" wrap="wrap" justify="center">
-          <ASPDoodle1 width="17rem" height="17rem" />
-          <ASPDoodle2 width="17rem" height="17rem" />
-        </Flex>
-        <Flex gap="2" wrap="wrap" justify="center">
-          <ASPDoodle3 width="17rem" height="17rem" />
-          <ASPDoodle4 width="17rem" height="17rem" />
-        </Flex>
-      </Flex>
+      <Separator size="4" />
+
+      <Heading size="8" align="center">
+        Other Projects
+      </Heading>
+
+      <Project
+        title="BlitzKit"
+        type={TimelineProjectType.Hobby}
+        time="2022 - present"
+      >
+        {lorem}
+      </Project>
+
+      <Project
+        title="Stellar"
+        type={TimelineProjectType.Hobby}
+        time="2021 - 2024"
+        images={["8z390aL", "Mgqhi9N"]}
+      >
+        My first proper endeavour into cyber-physical systems was the creation
+        of my own CAD software. Though 2-dimensional, I was pushing my
+        middle-school mathematical limits and researching capabilities beyond
+        what I had been formally taught.
+      </Project>
+
+      <Project
+        title="Avionics in Rocketry Club"
+        type={TimelineProjectType.Club}
+        time="2023"
+      >
+        {lorem}
+      </Project>
+
+      <Project
+        title="F-4 Phantom II Digitalization"
+        type={TimelineProjectType.Educational}
+        time="2023"
+      >
+        {lorem}
+      </Project>
     </>
   );
 }
